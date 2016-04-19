42fa2b17f1ede5823b85d3999b3e2b7ce5bdd55e
Gilson Silva <gilsonsilvati@gmail.com>
2016-04-19 18:01:51 -0300

    Configurando projeto com Spring Boot

---
+++ src/main/java/br/com/cds/DesafioDesenvolvedorJavaApplication.java
@@ -0,0 +1,12 @@
+package br.com.cds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioDesenvolvedorJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioDesenvolvedorJavaApplication.class, args);
+	}
+}
