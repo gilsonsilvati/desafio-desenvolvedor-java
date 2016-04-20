ef6ec4a55cae5ddbaa7b4c635880ce268d961c20
Gilson Silva <gilsonsilvati@gmail.com>
2016-04-20 17:50:31 -0300

    Configurando locale e enviando para o DB

--- src/main/java/br/com/cds/controller/PessoaController.java
+++ src/main/java/br/com/cds/controller/PessoaController.java
@@ -1,13 +1,29 @@
 package br.com.cds.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import br.com.cds.model.Pessoa;
+import br.com.cds.repository.Pessoas;
 
 @Controller
+@RequestMapping("/pessoas")
 public class PessoaController {
 	
-	@RequestMapping("/pessoas/novo")
+	@Autowired
+	private Pessoas pessoas;
+	
+	@RequestMapping("/novo")
 	public String novo() {
+		return "CadastroPessoa";
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public String salvar(Pessoa pessoa) {
+		pessoas.save(pessoa);
+		
 		return "CadastroPessoa";
 	}
 
