fc604ab07d670f4da6499089b6673193bb9651ff
Gilson Silva <gilsonsilvati@gmail.com>
2016-04-22 12:14:42 -0300

    Prototipando tela de pesquisa

--- src/main/java/br/com/cds/controller/PessoaController.java
+++ src/main/java/br/com/cds/controller/PessoaController.java
@@ -1,11 +1,18 @@
 package br.com.cds.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import br.com.cds.model.Pessoa;
+import br.com.cds.model.Status;
+import br.com.cds.model.UF;
 import br.com.cds.repository.Pessoas;
 
 @Controller
@@ -16,15 +23,33 @@ public class PessoaController {
 	private Pessoas pessoas;
 	
 	@RequestMapping("/novo")
-	public String novo() {
-		return "CadastroPessoa";
+	public ModelAndView novo() {
+		ModelAndView mv = new ModelAndView("CadastroPessoa");
+		return mv;
 	}
 	
 	@RequestMapping(method = RequestMethod.POST)
-	public String salvar(Pessoa pessoa) {
+	public ModelAndView salvar(Pessoa pessoa) {
 		pessoas.save(pessoa);
 		
-		return "CadastroPessoa";
+		ModelAndView mv = new ModelAndView("CadastroPessoa");
+		mv.addObject("mensagem", "Cadastro efetuado com sucesso!");
+		return mv;
+	}
+	
+	@RequestMapping
+	public String pesquisar() {
+		return "PesquisaPessoas";
+	}
+	
+	@ModelAttribute("todosUfPessoa")
+	private List<UF> todosUfPessoa() {
+		return Arrays.asList(UF.values());
+	}
+	
+	@ModelAttribute("todosStatusPessoa")
+	private List<Status> todosStatusPessoa() {
+		return Arrays.asList(Status.values());
 	}
 
 }
